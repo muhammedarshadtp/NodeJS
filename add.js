8ddd4266e79858859279dbd31859d3f8ccaeee9f
arshadtp <arshadtp89@gmail.com>
2025-10-27 20:43:42 +0530

    studying node.js

---
+++ add.js
@@ -0,0 +1,14 @@
+
+const add = (a,b)=>{
+    return a+b
+}
+
+const sub = (a,b)=>{
+    return a-b
+}
+
+const sum =add(2,4)
+
+console.log(sum);
+
+module.exports={sub,add}
---
+++ index.js
@@ -0,0 +1,8 @@
+const math=require('./add')
+
+console.log("hello World");
+
+const sum1=math.sub(5,2)
+const sum=math.add(2,3)
+console.log(sum);
+console.log(sum1);
