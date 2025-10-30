38e41c4460b72e2fa7884232303601a53565c523
arshadtp <arshadtp89@gmail.com>
2025-10-31 01:59:50 +0530

    server running on express

--- sample.js
+++ sample.js
@@ -1,10 +1,24 @@
 
-const http = require("http")
+// const http = require("http")
 
-const server=http.createServer((req,res)=>{
-    res.write('Hello world This is from Kochi')
-    res.end()
+// const server=http.createServer((req,res)=>{
+//     res.write('Hello world This is from Kochi')
+//     res.end()
 
-})
+// })
 
-server.listen(3000,()=>console.log("Server running on port 3000"))
+// server.listen(3000,()=>console.log("Server running on port 3000"))
+
+// const express = require('express')
+
+// const app=express()
+
+// app.get('/',(req,res)=>{
+//     res.send("Hello Arshad")
+
+// });
+// app.get('/about',(req,res)=>{
+//     res.send("This is about page")
+// })
+
+// app.listen(3000,()=>{console.log("server is running");})
